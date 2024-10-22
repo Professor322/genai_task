eefc57024a64c5348ada0336081b8c948380c08c
Nikolai Kochetkov <professor3222@gmail.com>
2024-10-22 21:12:18 +0100

    fix serialization problem

--- training/loggers.py
+++ training/loggers.py
@@ -11,7 +11,7 @@ class WandbLogger:
                 "id": wandb.util.generate_id(),
                 "project": "genai_task",
                 "name": config['train']['model'],
-                "config": config['model_args']
+                "config": "test"
         }
 
         wandb.init(**self.wandb_args, resume="allow")
